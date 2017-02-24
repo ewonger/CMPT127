ef0f337cd84e7e8f78b4056debc56634f9ba17d1
Eric <ewa38@sfu.ca>
2017-02-24 12:17:06 -0800

    reverse is working

--- 2/reverse.c
+++ 2/reverse.c
@@ -1,11 +1,13 @@
 void reverse( int arr[],unsigned int len)
 {
   int i, k;
+  int n=len;
 
   for( i=0; i<len/2; i++)
   {
     k=arr[i];
-    arr[i]=arr[len-1];
-    arr[len-1]=k;
+    arr[i]=arr[n-1];
+    arr[n-1]=k;
+    n--;
   }
 }
