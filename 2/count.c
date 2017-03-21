bd432ef71576d7363e89c924d7bb16928788e369
Eric <ewa38@sfu.ca>
2017-03-21 11:44:23 -0700

    count.c is working

---
+++ 2/count.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( void )
+{
+  int ch, word=0, last;
+  long unsigned int charcount=0, wordcount=0, linecount=0;
+
+  while (EOF != (ch=getchar()))
+  {
+    charcount++;
+    if ((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch==39))
+    {
+      word=1;
+    }
+    else
+    {
+      word=0;
+    }
+    if (last==1 && word==0)
+    {
+      wordcount++;
+    }
+    last=word;
+    if (ch=='\n')
+    {
+      linecount++;
+      charcount--;
+    }
+  }
+  printf("%lu %lu %lu\n", charcount, wordcount, linecount );
+  return 0;
+}
