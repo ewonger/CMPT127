66bd38deb4bdb7d89d926faea62798375a24e608
Eric <ewa38@sfu.ca>
2017-03-25 23:53:20 -0700

    censored.c is working

---
+++ 2/censored.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main( int argc, char* argv[] )
+{
+  int ch;
+  int found=0;
+  int i=0, n=0;
+  char tempArr[128];
+
+  while (EOF!=(ch=getchar()))
+  {
+    if ((ch>=65 && ch<=90) || (ch>=97 && ch<=122) || (ch==39))
+    {
+      tempArr[i]=ch;
+      i++;
+    }
+    else
+    {
+      found=0;
+      for (n=1;n<argc;n++)
+      {
+        if (strcmp(tempArr,argv[n])==0)
+        {
+          found=1;
+          printf("CENSORED");
+        }
+      }
+      if(found==0)
+      {
+        printf("%s",tempArr);
+      }
+      printf("%c",ch);
+      memset(tempArr,0,128);
+      i=0;
+    }
+  }
+  return 0;
+}
