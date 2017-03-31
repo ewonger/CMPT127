f0113b668e9a2d0d349619ab54c7034543aa7bf6
Eric <ewa38@sfu.ca>
2017-03-30 19:31:50 -0700

    resubmitting inrect.c

--- 2/inrect.c
+++ 2/inrect.c
@@ -10,12 +10,10 @@ int InRectangle( float pt[2], float rect[4] )
     {
       if (pt[1]>=rect[1]&&pt[1]<=rect[3])
       {
-        printf("y=1\n");
         flagy=1;
       }
       if (pt[0]<=rect[2]&&pt[0]>=rect[0])
       {
-        printf("x=1\n");
         flagx=1;
       }
     }
@@ -23,12 +21,10 @@ int InRectangle( float pt[2], float rect[4] )
     {
       if (pt[1]>=rect[3]&&pt[1]<=rect[1])
       {
-        printf("y=1\n");
         flagy=1;
       }
       if (pt[0]<=rect[2]&&pt[0]>=rect[0])
       {
-        printf("x=1\n");
         flagx=1;
       }
     }
@@ -39,12 +35,10 @@ int InRectangle( float pt[2], float rect[4] )
     {
       if (pt[1]>=rect[1]&&pt[1]<=rect[3])
       {
-        printf("y=1\n");
         flagy=1;
       }
       if (pt[0]<=rect[0]&&pt[0]>=rect[2])
       {
-        printf("x=1\n");
         flagx=1;
       }
     }
@@ -52,24 +46,20 @@ int InRectangle( float pt[2], float rect[4] )
     {
       if (pt[1]>=rect[3]&&pt[1]<=rect[1])
       {
-        printf("y=1\n");
         flagy=1;
       }
       if (pt[0]<=rect[0]&&pt[0]>=rect[2])
       {
-        printf("x=1\n");
         flagx=1;
       }
     }
   }
   if (flagx==1 && flagy==1)
   {
-    printf("returned 1");
     return 1;
   }
   else
   {
-    printf("returned 0");
     return 0;
   }
 }
