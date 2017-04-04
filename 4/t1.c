4c26e390487eba917117906b437df003aef12fc9
ewa38 <ewa38@asb9838nu-b04.csil.sfu.ca>
2017-04-04 12:47:07 -0700

    resubmitting t1.c

--- 4/t1.c
+++ 4/t1.c
@@ -12,43 +12,38 @@ void draw_circle( uint8_t img[],
 		              int r,
 		              uint8_t color )
 {
-  int i=0,k=0,index,left=x-r,right=x+r,top=y-r,bottom=y+r;
+  int i=0,k=0,index;
   double htopleft,htopright,hbotleft,hbotright;
   if (r>0)
   {
-    if (left<0)
+    for (i=y-r;i<=y+r;i++)
     {
-      left=0;
-    }
-    if (top<0)
-    {
-      top=0;
-    }
-    for (i=top;i<=bottom;i++)
-    {
-      if (i>rows)
+      if (i<0)
+      {
+        i=0;
+      }
+      if (i>=rows+r)
       {
         break;
       }
-      for (k=left;k<=right;k++)
+      for (k=x-r;k<=x+r;k++)
       {
-
+        if (k<0)
+        {
+          k=0;
+        }
         index=k+i*cols;
         htopleft=hypot(k-0.5-x,i-0.5-y);
         htopright=hypot(k+0.5-x,i+0.5-y);
         hbotleft=hypot(k-0.5-x,i+0.5-y);
         hbotright=hypot(k+0.5-x,i-0.5-y);
-        printf("%d %d %d\n",i,k,cols);
-        if (k==cols||k==2*cols)
+        if (k==cols)
         {
           break;
         }
-        else
+        if (htopleft<r||htopright<r||hbotleft<r||hbotright<r)
         {
-          if (htopleft<r||htopright<r||hbotleft<r||hbotright<r)
-          {
-            img[index]=color;
-          }
+          img[index]=color;
         }
       }
     }
