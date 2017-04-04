43c5a8fa8e82e1c802d5f6b36993323c7c8dd378
ewa38 <ewa38@asb9838nu-b04.csil.sfu.ca>
2017-04-04 12:05:21 -0700

    submitting t2.c

---
+++ 4/t2.c
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void draw_rectangle( uint8_t array[],
+		          unsigned int cols,
+		          unsigned int rows,
+		          int x,
+		          int y,
+		          int rect_width,
+		          int rect_height,
+		          uint8_t color )
+{
+  int i=0,k=0,index;
+
+  if (rect_width!=0||rect_height!=0)
+  {
+    if (rect_width<0||rect_height<0)
+    {
+      if (rect_width<0)
+      {
+        rect_width=rect_width*-1;
+      }
+      if (rect_height<0)
+      {
+        rect_height=rect_height*-1;
+      }
+      for (i=y-rect_height;i<=y;i++)
+      {
+        if (i<0)
+        {
+          i=0;
+        }
+        for (k=x-rect_width;k<=x;k++)
+        {
+          if (k<0)
+          {
+            k=0;
+          }
+          index=k+i*cols;
+          if (k==cols)
+          {
+            break;
+          }
+          if (i==y-rect_height||i==y||k==x-rect_width||k==x)
+          {
+            array[index]=color;
+          }
+        }
+      }
+    }
+    else if (rect_width>0&&rect_height>0)
+    {
+      for (i=y;i<=y+rect_height;i++)
+      {
+        if (i<0)
+        {
+          i=0;
+        }
+        for (k=x;k<=x+rect_width;k++)
+        {
+          if (k<0)
+          {
+            k=0;
+          }
+          index=k+i*cols;
+          if (k==cols)
+          {
+            break;
+          }
+          if (i==y||i==y+rect_height||k==x||k==x+rect_width)
+          {
+            array[index]=color;
+          }
+        }
+      }
+    }
+  }
+}
