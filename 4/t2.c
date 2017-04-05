6073737f6b26d1c5a300e5e2d8956f4b59a1f146
Eric <ewa38@sfu.ca>
2017-04-04 23:27:10 -0700

    resub t2.c

--- 4/t2.c
+++ 4/t2.c
@@ -14,19 +14,10 @@ void draw_rectangle( uint8_t array[],
 {
   int i=0,k=0,index;
 
-  if (rect_width<0||rect_height<0)
+  if (rect_width<0&&rect_height<0)
   {
-    if (rect_width<0)
-    {
-      rect_width=rect_width*-1;
-    }
-    if (rect_height<0)
-    {
-      rect_height=rect_height*-1;
-    }
-
-		rect_height=rect_height-1;
-		rect_width=rect_width-1;
+		rect_height=(rect_height*-1)-1;
+		rect_width=(rect_width*-1)-1;
     for (i=y-rect_height;i<=y;i++)
     {
 			if (i>=0)
@@ -34,17 +25,65 @@ void draw_rectangle( uint8_t array[],
       	for (k=x-rect_width;k<=x;k++)
       	{
         	index=k+i*cols;
-					if (index<0||index>cols*rows||k>=cols)
+					if (index>0&&index<cols*rows&&k<=cols)
 					{
-						break;
+						if (k>=0)
+						{
+        			if (i==y-rect_height||i==y||k==x-rect_width||k==x)
+        			{
+          			array[index]=color;
+        			}
+						}
 					}
-					if (k>=0)
+				}
+    	}
+  	}
+	}
+	else if (rect_width>0&&rect_height<0)
+  {
+		rect_height=(rect_height*-1)-1;
+		rect_width=rect_width-1;
+    for (i=y-rect_height;i<=y;i++)
+    {
+			if (i>=0)
+			{
+      	for (k=x;k<=x+rect_width;k++)
+      	{
+        	index=k+i*cols;
+					if (index>0&&index<cols*rows&&k<=cols)
 					{
-        		if (i==y-rect_height||i==y||k==x-rect_width||k==x)
-        		{
-							printf("(%d,%d)\n",k,i);
-          		array[index]=color;
-        		}
+						if (k>=0)
+						{
+        			if (i==y-rect_height||i==y||k==x+rect_width||k==x)
+        			{
+          			array[index]=color;
+        			}
+						}
+					}
+				}
+    	}
+  	}
+	}
+	else if (rect_width<0&&rect_height>0)
+  {
+		rect_height=rect_height-1;
+		rect_width=(rect_width*-1)-1;
+    for (i=y;i<=y+rect_height;i++)
+    {
+			if (i>=0)
+			{
+      	for (k=x-rect_width;k<=x;k++)
+      	{
+        	index=k+i*cols;
+					if (index>0&&index<cols*rows&&k<=cols)
+					{
+						if (k>=0)
+						{
+        			if (i==y+rect_height||i==y||k==x-rect_width||k==x)
+        			{
+          			array[index]=color;
+        			}
+						}
 					}
 				}
     	}
@@ -61,20 +100,18 @@ void draw_rectangle( uint8_t array[],
       	for (k=x;k<=x+rect_width;k++)
       	{
         	index=k+i*cols;
-					if (index<0||index>cols*rows||k>=cols)
+					if (index>0&&index<cols*rows&&k<=cols)
 					{
-						break;
-					}
 					if (k>=0)
 					{
         		if (i==y||i==y+rect_height||k==x||k==x+rect_width)
         		{
-							printf("(%d,%d)\n",k,i);
           		array[index]=color;
         		}
 			  	}
+					}
       	}
-			}
+			 }
     }
   }
 }
