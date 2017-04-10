0f3053904d1cac60a8000b70133e4905eb63b412
Eric <ewa38@sfu.ca>
2017-04-09 21:55:05 -0700

    resub t2.c

--- 6/t2.c
+++ 6/t2.c
@@ -9,28 +9,30 @@
 int intarr_save_json( intarr_t* ia, const char* filename )
 {
   int i=0;
-	if (ia != NULL)
+	if (ia == NULL)
   {
-		FILE* f=fopen(filename,"w");
-		if (f!= NULL)
+    return 1;
+  }
+	FILE* f=fopen(filename,"w");
+	if (f== NULL)
+  {
+    return 1;
+  }
+	fprintf(f,"[\n");
+	for (i=0; i<ia->len; i++)
+  {
+		if (i==ia->len-1)
     {
-			fprintf(f,"[\n");
-			for (i=0; i<ia->len; i++)
-      {
-				if (i == ia->len-1)
-        {
-					fprintf(f," %d\n",ia->data[i]);
-				}
-				else{
-					fprintf(f," %d,\n",ia->data[i]);
-				}
-			}
-		fprintf(f,"]");
-		fclose(f);
-		return 0;
+			fprintf(f," %d\n",ia->data[i]);
+		}
+		else
+    {
+			fprintf(f," %d,\n",ia->data[i]);
 		}
 	}
-	return 1;
+  fprintf(f,"]");
+  fclose(f);
+  return 0;
 }
 
 intarr_t* intarr_load_json( const char* filename )
@@ -44,7 +46,7 @@ intarr_t* intarr_load_json( const char* filename )
 	}
 	intarr_t* createarr=intarr_create(0);
 	fscanf(f,"%d ",&num);
-	while (fgets(arr,100,f))
+	while (fgets(arr,1000,f))
   {
 		if (arr[0] == ']'||scanf(arr,"%d",&num)!=1)
     {
